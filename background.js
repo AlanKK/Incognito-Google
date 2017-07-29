f66203f8f5635acb6064b9f0ce983f44e34f6309
AlanKK <AlanKK@users.noreply.github.com>
2017-07-29 10:20:25 -0400

    google maps search fix
    Change so searches within google maps don't redirect to incognito

--- background.js
+++ background.js
@@ -1,22 +1,40 @@
 function findLastIncognito(windows) {
-    var win = false;
+    var win = 0;
     for(var i = windows.length - 1; i >= 0; i--)
-        if (windows[i].incognito) win = windows[i];
+        if (windows[i].incognito) {
+            // This extension can't see incognito windows unless it is running in incognito
+            // mode.  When that is enabled, we need to prevent tab open spam.  
+            var tabs = windows[i].tabs;
+            for (var j = tabs.length - 1; j >= 0; j--) {
+                if (tabs[j].id == tabId) return -1;
+            }
+            win = windows[i];
+        }
     return win;
 }
 
 function openGoogleLink(thisUrl, putInThisWindow) {
-    if (putInThisWindow)
-        chrome.tabs.create({ windowId: putInThisWindow.id, url: thisUrl, selected: false});
-    else
+    if (putInThisWindow == -1) {
+        return;
+    } else if (putInThisWindow == 0) {
+        console.log("No existing incog window found.");
         chrome.windows.create({ url: thisUrl, incognito: true});
+    }
+    else {
+        console.log("Found existing incog window.");
+        chrome.tabs.create({ windowId: putInThisWindow.id, url: thisUrl, selected: false});
+    }
 }
 
+var tabId = -1;
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         var url = details['url'];
-        var tabId = details['tabId'];
+        tabId = details['tabId'];
 
+		// Don't redirect maps searches at maps.google.com
+		if (url.indexOf('search?tbm=map') !== -1) return;
+		
         if (url.substr(0, 1) === '/' && url.substr(0, 2) !== '//')
           url = 'https://www.google.com' + url;
 
